ea88efe0a32e0a138e06105110c422346e239dc9
Noriskky <110040212+Noriskky@users.noreply.github.com>
2024-05-03 20:48:31 +0200

    Changed Name 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tls",
+    name="Temp-Linux-System",
     version="0.1.0",
     author="Noriskky",
     author_email="Noriskky44@proton.me",
