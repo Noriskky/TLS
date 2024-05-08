faaf03745b5c4ab5df5c1ea3878fb8aa8eba0248
Noriskky <110040212+Noriskky@users.noreply.github.com>
2024-05-09 00:52:35 +0200

    Changed Version Number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="TLS",
-    version="0.1.3",
+    version="0.2-Beta",
     author="Noriskky",
     platforms=["Linux"],
     author_email="Noriskky44@proton.me",
