82ae556e68a76aa6ec86a75910dd844fc58aa3c6
Noriskky <110040212+Noriskky@users.noreply.github.com>
2024-05-13 21:57:13 +0200

    IDK what the Difference is xD

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: POSIX :: Linux",
+        "Operating System :: POSIX :: Linux"
     ],
     install_requires=[
         "requests",
