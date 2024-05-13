0fd5cf156b879bdfe4250170feba5aafa16bfe4e
Noriskky <110040212+Noriskky@users.noreply.github.com>
2024-05-13 22:03:22 +0200

    Changed Version Number
    Ups forgot to Change the version Number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Temp-Linux-Shell",
-    version="0.2.1-Beta",
+    version="1.0-Release",
     author="Noriskky",
     platforms=["Linux"],
     author_email="Noriskky44@proton.me",
