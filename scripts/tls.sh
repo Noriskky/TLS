542fbec9ec5d948af8832cb96de76d1963718efc
Noriskky <110040212+Noriskky@users.noreply.github.com>
2024-05-13 21:25:11 +0200

    Added Test Script

---
+++ scripts/tls.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+min=10
+max=100
+random=$((RANDOM % (max - min + 1) + min))
+CWD="/var/tmp/tls-sh-$random"
+
+PYTHON=$CWD/bin/python
+PIP=$CWD/bin/pip
+TLS=$CWD/bin/tls
+
+python -m venv $CWD
+
+$PIP install Temp-Linux-Shell
+$TLS
